42ed21dcbdc128f00625b5784488afbc377412fe
Sukhnain Deol <dsukhnain@gmail.com>
2023-02-14 15:18:47 -0800

    started

--- WordSearchGenerator.java
+++ WordSearchGenerator.java
@@ -1,1 +1,18 @@
+// Programmer: Sukhnain Deol
+// Class: CS 145
+// Date: 02/14/2023
+// Assignmnet: Lab 5: Tower Of Hanoi
 
+// Purpose: 
+
+// Notes
+    //Min moves scales by (2^NUM_OF_DISKS)-1
+
+// To DO:
+    // pre/post condition
+    // everything
+
+class WordSearchGenerator 
+{
+
+} // end of WordSearchGenerator class
