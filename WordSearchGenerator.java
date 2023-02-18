3cbbcc7bd8504a8b3d587ebfd32ca642ed42f2af
Sukhnain Deol <dsukhnain@gmail.com>
2023-02-18 10:17:20 -0800

    Revert "a"
    This reverts commit b154a805257b94fc6ebfc654bd6155ee1dc5ed82.

--- WordSearchGenerator.java
+++ WordSearchGenerator.java
@@ -18,21 +18,13 @@
     // comments 
     // cleanup
     // <84 char
-    // procedure
-        // choose random coord
-        // choose random direction
-        // check if enough space & no other letters there
-        // recursive back tracking 100 times
-    // account for dynamic size
-        // width = longest word or length of # of words
-    // use a stack
 
 import java.util.*;
 
 class WordSearchGenerator 
 {
-    char[][] wordSearch = new char[10][10];
-    char[][] solvedWordSearch = new char[10][10];
+    char[][] wordSearch;
+    char[][] solvedWordSearch;
 
     // prob not needed
     public WordSearchGenerator()
@@ -46,7 +38,28 @@ class WordSearchGenerator
     // the words are and generate a word search based on those words
     public void generate(ArrayList<String> searchWords)
     {
-        
+        // turn into findLongest mehod?
+        int longest = searchWords[0].length();
+        // loops each element after 0
+        for (int i = 1; i < searchWords.length; i++)
+        {   // if element has longer length than current, reassign
+            if(searchWords[i].length() > longest)
+                {longest = searchWords[i].length();}
+        }
+        // make it at least enough space for all words?
+        solvedWordSearch = new int[longest][longest];
+        // iterate over each array
+        for(char[] i : arr)
+        {
+            // iterate over each element in each array
+            for(int j: i)
+            {   
+                //  each element in each array
+                System.out.print(i[j] + " ");
+            }
+            System.out.println(); // next line after each array 
+        }
+
     } // end of generate method
 
 
